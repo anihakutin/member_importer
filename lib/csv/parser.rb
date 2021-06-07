e54eadcdb27c99057fa223c092e3ad214960f3a3
Heshie <4hbrodygmail.com>
2021-06-07 01:33:24 -0400

    Check csv for rows with nill values

--- lib/csv/parser.rb
+++ lib/csv/parser.rb
@@ -7,9 +7,16 @@ module Csv
         @input_file = CSV.open(path)
       rescue => e
         raise ArgumentError.new(
-           "Could not open CSV file at provided location. #{e}"
+           "Could not open CSV file with the provided path. #{e}"
          )
       end
+    end
+
+    def is_row_valid?(row)
+      return false unless row && row.kind_of?(Array)
+      return false if row.any? { |v| v.nil? || v.strip.empty? }
+
+      return true
     end
   end
 end
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -1,9 +1,31 @@
 RSpec.describe Csv::Parser do
-  it "instantiates a csv parser with a csv file" do
+  path = File.expand_path('../../data/input.csv', __FILE__)
 
-    path = File.expand_path('../../data/input.csv', __FILE__)
+  it "instantiates a csv parser with a csv file" do
     csv_parser = Csv::Parser.new(path)
 
     expect(csv_parser).to be_an_instance_of(Csv::Parser)
+  end
+
+  it "rejects rows where not all values are present" do
+    csv_parser = Csv::Parser.new(path)
+    invalid_data_row = ['Jason','Bateman ','12/12/2010','AB 0000','','','']
+
+    expect(csv_parser.is_row_valid?(invalid_data_row)).to eq(false)
+  end
+
+  it "accepts rows where all values are present" do
+    csv_parser = Csv::Parser.new(path)
+    valid_data_row = [
+      'Antonio',
+      'Brown ',
+      '2/2/1966',
+      '890887',
+      '9/30/19',
+      '9/30/2000',
+      '303-333-9987'
+    ]
+
+    expect(csv_parser.is_row_valid?(valid_data_row)).to eq(true)
   end
 end
