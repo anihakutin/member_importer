70ff00e36341a572a961f5b3ed18b494e63c2e61
Heshie <4hbrodygmail.com>
2021-06-07 01:09:38 -0400

    Parser init or raise error. Update tests input file path

--- lib/csv/parser.rb
+++ lib/csv/parser.rb
@@ -2,5 +2,14 @@ module Csv
   class Parser
     require 'csv'
 
+    def initialize(path)
+      begin
+        @input_file = CSV.open(path)
+      rescue => e
+        raise ArgumentError.new(
+           "Could not open CSV file at provided location. #{e}"
+         )
+      end
+    end
   end
 end
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Csv::Parser do
   it "instantiates a csv parser with a csv file" do
 
-    path = File.expand_path('../data/input.csv', __FILE__)
+    path = File.expand_path('../../data/input.csv', __FILE__)
     csv_parser = Csv::Parser.new(path)
 
     expect(csv_parser).to be_an_instance_of(Csv::Parser)
