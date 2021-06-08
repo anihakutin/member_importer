e9bc298b1d61551ef514707647fcfb7dbfa7240a
Heshie <4hbrodygmail.com>
2021-06-08 00:47:06 -0400

    Convert 2 digit year. Write csv.

--- lib/csv/parser.rb
+++ lib/csv/parser.rb
@@ -2,16 +2,7 @@ module Csv
   class Parser
     require 'csv'
     require 'date'
-
-    REQUIRED_HEADER = [
-      'last_name',
-      'first_name',
-      'dob',
-      'member_id',
-      'effective_date',
-      'expiry_date',
-      'phone_number'
-    ]
+    require 'pry'
 
     def initialize(path)
       begin
@@ -21,16 +12,40 @@ module Csv
            "Could not open CSV file with the provided path. #{e}"
          )
       end
+
+      @required_header = [
+        'first_name',
+        'last_name',
+        'dob',
+        'member_id',
+        'effective_date',
+        'expiry_date',
+        'phone_number'
+      ]
+
+      @output_header= [
+        'last_name',
+        'first_name',
+        'dob',
+        'member_id',
+        'effective_date',
+        'expiry_date',
+        'phone_number'
+      ]
     end
 
     def parse
       valid_rows = []
       invalid_rows = []
 
-      header_row = @input_file[0]
+      csv_rows = @input_file.each.to_a.compact
+      header_row = csv_rows[0]
 
       if is_header_valid?(header_row)
-        @input_file.shift.each_with_index do |row, i|
+        csv_rows.each_with_index do |row, i|
+          # skip header
+          next if i == 0
+
           error_messages = []
 
           error_messages << "has missing required values." unless is_row_valid?(row)
@@ -53,7 +68,7 @@ module Csv
     def is_header_valid?(header_row)
       return false if header_row.empty?
 
-      return REQUIRED_HEADER == header_row
+      return @required_header.difference(header_row).any?
     end
 
     def is_row_valid?(row)
@@ -64,6 +79,8 @@ module Csv
     end
 
     def is_phone_valid?(phone_number)
+      return false if phone_number.nil? || phone_number.empty?
+
       formatted_number = phone_number.gsub(/([-+() ])/, '')
 
       return true if [10, 11].include?(formatted_number.length)
@@ -79,24 +96,76 @@ module Csv
     end
 
     def is_date_valid?(date)
+      return false if date.nil? || date.empty?
+      counter = 0
+
       begin
-        Date.parse(date).strftime('%F')
+        counter += 1
+
+        if counter > 1
+          date_str = convert_two_digit_year(date)
+          new_date = Date.strptime(date_str, '%m/%d/%Y')
+          new_date.strftime('%F')
+        else
+          Date.parse(date).strftime('%F')
+        end
 
         return true
       rescue
+        retry if counter < 2
+      else
         return false
       end
     end
 
+    def convert_two_digit_year(date)
+      year = date.split("/").last.to_i
+      year += 2000 if (0..29).include?(year)
+      year += 1900 if (30..99).include?(year)
+
+      new_date = date.split("/")
+      new_date[2] = year
+
+      return new_date.join("/")
+    end
+
     def format_date(date)
-      return Date.parse(date).strftime('%F')
+      begin
+        Date.parse(date).strftime('%F')
+      rescue
+        date_str = convert_two_digit_year(date)
+        new_date = Date.strptime(date_str, '%m/%d/%Y')
+        new_date.strftime('%F')
+      end
     end
 
     def write_csv(rows)
+      return if rows.empty?
 
+      formatted_rows = []
+      rows.each do |row|
+        # Put the last name first
+        row[0], row[1] = row[1], row[0]
+
+        # Format dates
+        [2, 4, 5].each { |i| row[i] = format_date(row[i]) }
+
+        # Format phone number
+        row[6] = format_phone_number(row[6])
+
+        # Remove extra whitespace
+        formatted_rows << row.each.map(&:strip)
+      end
+
+      CSV.open("output/export.csv", "wb") do |csv|
+        csv << @output_header
+        formatted_rows.each { |row| csv << row }
+      end
     end
 
     def write_report(rows)
+      return if rows.empty?
+
 
     end
   end
