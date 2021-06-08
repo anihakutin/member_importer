6b5a0c6d851b65e6b0939e8fd8faa7045832e6b3
Heshie <4hbrodygmail.com>
2021-06-07 21:40:45 -0400

    Parse csv contents. Flag invalid rows.

--- lib/csv/parser.rb
+++ lib/csv/parser.rb
@@ -3,6 +3,16 @@ module Csv
     require 'csv'
     require 'date'
 
+    REQUIRED_HEADER = [
+      'last_name',
+      'first_name',
+      'dob',
+      'member_id',
+      'effective_date',
+      'expiry_date',
+      'phone_number'
+    ]
+
     def initialize(path)
       begin
         @input_file = CSV.open(path)
@@ -11,6 +21,39 @@ module Csv
            "Could not open CSV file with the provided path. #{e}"
          )
       end
+    end
+
+    def parse
+      valid_rows = []
+      invalid_rows = []
+
+      header_row = @input_file[0]
+
+      if is_header_valid?(header_row)
+        @input_file.shift.each_with_index do |row, i|
+          error_messages = []
+
+          error_messages << "has missing required values." unless is_row_valid?(row)
+          error_messages << "has an invalid date." unless is_date_valid?(row[2])
+          error_messages << "has an invalid date." unless is_date_valid?(row[4])
+          error_messages << "has an invalid date." unless is_date_valid?(row[5])
+          error_messages << "has an invalid phone number." unless is_phone_valid?(row[6])
+
+          valid_rows << row if error_messages.empty?
+          invalid_rows << [i, error_messages] if error_messages.any?
+        end
+      else
+        invalid_rows << [0, ["Header row is invalid."]]
+      end
+
+      write_csv(valid_rows)
+      write_report(invalid_rows)
+    end
+
+    def is_header_valid?(header_row)
+      return false if header_row.empty?
+
+      return REQUIRED_HEADER == header_row
     end
 
     def is_row_valid?(row)
@@ -47,6 +90,14 @@ module Csv
 
     def format_date(date)
       return Date.parse(date).strftime('%F')
+    end
+
+    def write_csv(rows)
+
+    end
+
+    def write_report(rows)
+
     end
   end
 end
